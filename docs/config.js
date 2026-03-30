2c0e02115451cd23ce6d0592a18a2b1bad5f9e10
jalentized-blip <jalentized-blip@users.noreply.github.com>
2026-03-30 17:56:27 -0500

    Update VERIFY_DOMAIN to TasteWP URL

--- docs/config.js
+++ docs/config.js
@@ -2,4 +2,4 @@
 // UPDATE THIS URL WHEN YOUR DOMAIN CHANGES
 // Then commit + push to GitHub — all QR codes update
 // =====================================================
-var VERIFY_DOMAIN = "https://vitalpeppro.com";
+var VERIFY_DOMAIN = "https://strengthenfriend.s6-tastewp.com";
